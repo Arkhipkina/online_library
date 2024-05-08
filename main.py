b19291bc0d5919e00688a0b5360740dd378abaa2
Arkhipkina <106983092+Arkhipkina@users.noreply.github.com>
2024-05-08 17:36:59 +0300

    download comments

--- main.py
+++ main.py
@@ -28,15 +28,14 @@ def download_txt(response, filename, folder="books"):
     return filepath
 
 
-def get_url_image(response, title):
+def get_url_image(response):
     soup = BeautifulSoup(response.text, 'lxml')
-    img = soup.find(class_="bookimage").find("img")["src"]
-    imgpath = urljoin("https://tululu.org/", img)
-    print(f"Заголовок: {title} \n {imgpath} \n")
+    img_tag = soup.find(class_="bookimage").find("img")["src"]
+    imgpath = urljoin("https://tululu.org/", img_tag)
     return imgpath
 
 
-def dowload_image(imgpath, filename, folder="image_books"):
+def dowload_image(imgpath, filename, folder="books_image"):
     response = requests.get(imgpath)
     response.raise_for_status()
     filepath = os.path.join(folder, filename)
@@ -44,32 +43,41 @@ def dowload_image(imgpath, filename, folder="image_books"):
         file.write(response.content)
 
 
+def get_comments(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    comments_tag = soup.find_all(class_="texts")
+    all_comments = [comment_book.find(class_="black").text for comment_book in comments_tag]
+    print(all_comments)
+
+
 
 def main():
     Path("books").mkdir(parents=True, exist_ok=True)
-    Path("image_books").mkdir(parents=True, exist_ok=True)
+    Path("books_image").mkdir(parents=True, exist_ok=True)
+    Path("books_comments").mkdir(parents=True, exist_ok=True)
 
 
     for id in range(1, 11):
-        url = f"https://tululu.org/txt.php?id={id}"
+        url_for_downloading = f"https://tululu.org/txt.php?id={id}"
         filename = f'books/book_{id}.txt'
-        response = requests.get(url)
-        response.raise_for_status()
+        response_for_downloading = requests.get(url_for_downloading)
+        response_for_downloading.raise_for_status()
         try:
-            check_for_redirect(response)
+            check_for_redirect(response_for_downloading)
         except requests.exceptions.HTTPError:
             continue
-        url_for_name = f"https://tululu.org/b{id}"
-        response_for_name = requests.get(url_for_name)
-        response_for_name.raise_for_status()
-        filename = get_books_name(response_for_name)
-        download_txt(response, f"{id}. {filename}.txt")
-        imgpath = get_url_image(response_for_name, filename)
+        page_url = f"https://tululu.org/b{id}"
+        page_response = requests.get(page_url)
+        page_response.raise_for_status()
+        filename = get_books_name(page_response)
+        download_txt(page_response, f"{id}. {filename}.txt")
+        imgpath = get_url_image(page_response)
         if imgpath == "https://tululu.org/images/nopic.gif":
             filename = "nopic.gif"
         else:
             filename = f"{id}.png"
         dowload_image(imgpath, filename)
+        get_comments(page_response)
 
 
 if __name__ == "__main__":
